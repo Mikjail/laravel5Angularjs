1e35e523d6e5ffaaef318e48ef1a0c0ecd841fd8
Mikjail Salazar <mikjailsalazar@Mikjails-MacBook-Pro.local>
2018-01-07 00:05:56 +0400

    fixing phone number

--- Runner/gulp/dist.js
+++ Runner/gulp/dist.js
@@ -6,7 +6,7 @@ var gModule = require("./gulpModules.js")();
 var distConfig = require('./distConfig.js')();
 var libConfig = require("./gulpconfig.js")();
 
-var PRODVERSION = "v5.0";
+var PRODVERSION = "v6.0";
 console.log("TRBAJANDO SOBRE VERSION:" + PRODVERSION);
 //////////////////////////////////////////////////////
 //                    HTML-TEMPLATE-CACHE           //
